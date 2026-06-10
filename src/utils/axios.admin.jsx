409b50314619170eb12cf64ce037636fe2662379
LOC-DEV71 <loclc8533@ut.edu.vn>
2026-06-10 22:19:30 +0700

    update api vite url

--- src/utils/axios.admin.jsx
+++ src/utils/axios.admin.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosAdmin = axios.create({
   // baseURL: "https://floren-x-v2-backend-xlj9.vercel.app/api/v1/admin",
-  baseURL: "http://localhost:3000/api/v1/admin",
+  baseURL: import.meta.env.VITE_API_URL_ADMIN||"http://localhost:3000/api/v1/admin",
   withCredentials: true
 });
 
--- src/utils/axios.client.jsx
+++ src/utils/axios.client.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   // baseURL: "https://floren-x-v2-backend-xlj9.vercel.app/api/v1/client",
-  baseURL: "http://localhost:3000/api/v1/client",
+  baseURL: import.meta.env.VITE_API_URL_ADMIN||"http://localhost:3000/api/v1/client",
   headers: {
     "Content-Type": "application/json"
   },
